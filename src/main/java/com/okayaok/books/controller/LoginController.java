e553ff7979fb256f3bd921fd43f95e00c43e9552
hang_xiao <xiao.hang@transwarp.cn>
2017-07-10 10:45:27 +0800

    修改用户登陆功能

--- src/main/java/com/okayaok/books/controller/LoginController.java
+++ src/main/java/com/okayaok/books/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.okayaok.books.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
  *         2017/7/8.
  */
 @RestController
-@RequestMapping("/")
 public class LoginController {
-
-    public void login() {
-       System.out.println("开始跳转至登陆页面");
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        System.out.println("开始跳转至登陆页面");
+        return "login";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void login(String username, String password) {
         System.out.println("登陆成功");
     }
--- src/main/java/com/okayaok/books/user/User.java
+++ src/main/java/com/okayaok/books/user/User.java
@@ -2,18 +2,47 @@ package com.okayaok.books.user;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author hang_xiao
  *         2017/7/8.
  */
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     private Integer id;
 
+    @NotNull
     private String username;
 
+    @NotNull
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
