4b78b0e5b8bc927d0b6280cded933c7e1f5d17f6
hang_xiao <xiao.hang@transwarp.cn>
2017-07-11 15:03:44 +0800

    修改登陆功能以及登录页面

--- src/main/java/com/okayaok/books/controller/LoginController.java
+++ src/main/java/com/okayaok/books/controller/LoginController.java
@@ -1,24 +1,28 @@
 package com.okayaok.books.controller;
 
+import com.okayaok.books.user.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author hang_xiao
  *         2017/7/8.
  */
-@RestController
+@Controller
 public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("user", new User());
         System.out.println("开始跳转至登陆页面");
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void login(String username, String password) {
+    public String login(User user) {
         System.out.println("登陆成功");
+        return "login";
     }
 }
