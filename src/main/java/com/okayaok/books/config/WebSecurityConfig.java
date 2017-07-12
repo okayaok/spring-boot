884cfe090cf4a749ee859f8f577d25c02a0651ff
hang_xiao <xiao.hang@transwarp.cn>
2017-07-12 15:31:37 +0800

    优化登陆页面的显示样式，优化静态资源的权限请求

--- src/main/java/com/okayaok/books/config/WebSecurityConfig.java
+++ src/main/java/com/okayaok/books/config/WebSecurityConfig.java
@@ -16,19 +16,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure (HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests()
-                    .antMatchers("/login", "/users/**").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+                .antMatchers("/static/**", "/login", "/users/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/")
-                    .permitAll()
-                    .and()
+                .loginPage("/")
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll();
+                .permitAll();
     }
 
     @Autowired
