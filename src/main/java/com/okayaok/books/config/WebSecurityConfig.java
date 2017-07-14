39fff6049ebb9f9153cb5010b335e7198658628e
hang_xiao <xiao.hang@transwarp.cn>
2017-07-14 16:14:02 +0800

    修改登录页面的跳转路径

--- src/main/java/com/okayaok/books/config/WebSecurityConfig.java
+++ src/main/java/com/okayaok/books/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/")
+                .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
