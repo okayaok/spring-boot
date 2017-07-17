96cb12a170cc328b3ef4f757c15a42308252bb3c
hang_xiao <xiao.hang@transwarp.cn>
2017-07-17 18:34:02 +0800

    增加密码加密方式

--- src/main/java/com/okayaok/books/config/WebSecurityConfig.java
+++ src/main/java/com/okayaok/books/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import com.okayaok.books.service.user.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Spring Security权限验证的配置
@@ -34,21 +38,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
+     * 设置密码的加密方式
+     *
+     * @return 加密方式
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    /**
      * 将自定义的UserDetailsService注入到认证管理中
      *
      * @param auth 认证授权
-     * @throws Exception
+     * @throws Exception 认证异常
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
+        auth.authenticationProvider(authenticationProvider());
     }
 
     /**
      * 配置路径的权限控制
      *
      * @param http 请求权限控制
-     * @throws Exception
+     * @throws Exception 认证请求异常
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -56,7 +78,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/static/**", "/login").permitAll()
-                .antMatchers("/", "/users/**").authenticated()
+                .antMatchers("/", "/users/**", "/roles/**").authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -72,7 +94,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      * 在内存中注入用户信息
      *
      * @param auth 认证授权
-     * @throws Exception
+     * @throws Exception 异常信息
      */
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
