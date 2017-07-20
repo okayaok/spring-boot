aad801b5fe41f4da560d09486bebdcc7e5239187
hang_xiao <xiao.hang@transwarp.cn>
2017-07-20 18:02:30 +0800

    修改提示信息的判断

--- src/main/java/com/okayaok/books/controller/LoginController.java
+++ src/main/java/com/okayaok/books/controller/LoginController.java
@@ -4,7 +4,6 @@ import com.okayaok.books.domain.User;
 import com.okayaok.books.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
