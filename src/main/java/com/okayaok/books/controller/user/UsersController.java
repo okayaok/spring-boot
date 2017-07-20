c8815a651b0c29b34070c99dcf47a2a8789c74b1
hang_xiao <xiao.hang@transwarp.cn>
2017-07-20 10:52:04 +0800

    添加默认的分页配置，修改分页的样式

--- src/main/java/com/okayaok/books/controller/user/UsersController.java
+++ src/main/java/com/okayaok/books/controller/user/UsersController.java
@@ -5,6 +5,7 @@ import com.okayaok.books.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,7 @@ public class UsersController {
      * 获取用户列表
      */
     @RequestMapping(method = RequestMethod.GET)
-    public String index(Model model, Pageable pageable) {
+    public String index(Model model, @PageableDefault Pageable pageable) {
         Page<User> users = usersRepository.findAll(pageable);
         model.addAttribute("users", users);
         return "user/index";
